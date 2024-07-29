a128e55c609a5d50b0ef84e4a0ca09a5d42119e5
Jakob Lochinski <38576440+jakobloch@users.noreply.github.com>
2024-07-28 23:30:00 -0400

    init

---
+++ odoo_venv/bin/runxlrd.py
@@ -0,0 +1,4 @@
+#!/Users/jakoblochinski/Documents/GitHub/odoo/odoo_venv/bin/python
+# EASY-INSTALL-SCRIPT: 'xlrd==2.0.1','runxlrd.py'
+__requires__ = 'xlrd==2.0.1'
+__import__('pkg_resources').run_script('xlrd==2.0.1', 'runxlrd.py')
---
+++ odoo_venv/bin/vba_extract.py
@@ -0,0 +1,4 @@
+#!/Users/jakoblochinski/Documents/GitHub/odoo/odoo_venv/bin/python
+# EASY-INSTALL-SCRIPT: 'XlsxWriter==3.2.0','vba_extract.py'
+__requires__ = 'XlsxWriter==3.2.0'
+__import__('pkg_resources').run_script('XlsxWriter==3.2.0', 'vba_extract.py')
